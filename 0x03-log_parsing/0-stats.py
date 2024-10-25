c450fb4de5309909c843ff667a20321ec05b1312
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-25 05:54:32 +0100

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,7 @@ import sys
 import signal
 
 
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 total_file_size = 0
 line_count = 0
@@ -16,9 +16,9 @@ def print_statistics():
     occurrences of each status code.
     """
     print(f"File size: {total_file_size}")
-    for code in sorted(status_code_counts.keys()):
-        if status_code_counts[code] > 0:
-            print(f"{code}: {status_code_counts[code]}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
 
 def handle_interrupt(signal, frame):
@@ -40,8 +40,8 @@ for line in sys.stdin:
         status_code = int(parts[-2])
         file_size = int(parts[-1])
         total_file_size += file_size
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
+        if status_code in status_codes:
+            status_codes[status_code] += 1
         line_count += 1
         if line_count % 10 == 0:
             print_statistics()
