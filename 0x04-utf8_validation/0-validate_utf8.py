f901026a1bb705caabbcc19a943befc548a252b8
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-31 23:10:31 +0100

    done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,27 +1,23 @@
-#!/usr/bin/env python3
-
-
+#!/usr/bin/python3
 def validUTF8(data):
     """
-    Determines if a given data set
-    represents a valid UTF-8 encoding.
+    Determines if a given data set represents a valid UTF-8 encoding.
 
     Args:
-        data (list): A list of integers,
-        each representing a byte of data.
+        data (list): A list of integers, each representing a byte of data.
 
     Returns:
-        bool: True if data is a valid UTF-8 encoding,
-        else False.
+        bool: True if data is a valid UTF-8 encoding, else False.
     """
     num_bytes = 0
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
 
     for byte in data:
-        byte = byte & 0xFF
+        byte = byte & 0xFF  # Consider only the last 8 bits
 
         if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
             mask = 1 << 7
             while mask & byte:
                 num_bytes += 1
@@ -32,6 +28,7 @@ def validUTF8(data):
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
+            # Check if byte follows the pattern 10xxxxxx
             if not (byte & mask1 and not (byte & mask2)):
                 return False
 
