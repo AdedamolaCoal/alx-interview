a02fe53168c66c21920da4dae568753f7868cc2c
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-31 23:02:41 +0100

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 
 def validUTF8(data):
@@ -15,27 +15,23 @@ def validUTF8(data):
         else False.
     """
     num_bytes = 0
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
     for byte in data:
-        byte = byte & 0xFF  # Keep only the last 8 bits
+        byte = byte & 0xFF
 
         if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
             mask = 1 << 7
             while mask & byte:
                 num_bytes += 1
                 mask >>= 1
 
-            # If it's a 1-byte character or invalid byte count
             if num_bytes == 0:
                 continue
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # For subsequent bytes, check if they
-            # follow the pattern 10xxxxxx
             if not (byte & mask1 and not (byte & mask2)):
                 return False
 
