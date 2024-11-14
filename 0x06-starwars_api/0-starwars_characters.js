3f3a91db9fde599ba6c908e0be9d6356b65ef43c
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-14 16:43:57 +0100

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie_ID>');
-  process.exit(1);
-}
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
+    if (error || response.statusCode !== 200) {
+        console.error('Error fetching data:', error);
         return;
-      }
+    }
 
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+    const data = JSON.parse(body);
+    const characterUrls = data.characters;
+
+    characterUrls.forEach(url => {
+        request(url, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const characterData = JSON.parse(body);
+                console.log(characterData.name);
+            }
+        });
     });
-  });
 });
